889989566157337258bb5d858c7547b4a711201d
Vikram Jadhav <vikramcse.10@gmail.com>
2015-12-07 12:04:36 +0000

    changed the mongoose model to store ip of user

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var port = process.env.port || 7777;
 var express = require("express");
+var cons = require('consolidate');
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var shortid = require("shortid");
+var requestIp = require('request-ip');
+var base_url = "http://192.168.33.10:7777";
 
 var db = mongoose.connect("mongodb://localhost/urlData");
 var Url = require("./urlModel");
@@ -12,6 +15,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
+app.engine('html', cons.swig)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/public')
+
 app.use(express.static(__dirname + './public'));
 
 app.get("/", function (req, res) {
@@ -19,23 +26,33 @@ app.get("/", function (req, res) {
 });
 
 app.post('/', function(req, res) {
+	var clientIp = requestIp.getClientIp(req).split(':')[3];
+	console.log(clientIp);
+
 	// Declare variables
     var url, id;
 
     // Get URL
-    url = 'www.youtube.com';
+    url = req.body.url;
 
     // Create a hashed short version
     id = shortid.generate();
 
     var newUrl = Url({
     	url: url,
-    	segment: id
+    	segment: id,
+    	url_ip: clientIp
     });
 
     newUrl.save(function(err) {
-	  if (err) throw err;
-	  console.log('Url created!');
+		if (err) throw err;
+
+		res.render('output', {
+			id: id,
+			base_url: base_url 
+		});
+
+		console.log('Url created!');
 	});
 });
 
