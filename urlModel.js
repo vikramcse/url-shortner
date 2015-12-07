3cb8ae3f509ebc80a0c57dfd377028f3a7ee6a30
Vikram Jadhav <vikramcse.10@gmail.com>
2015-12-07 12:05:08 +0000

    changed the mongoose model to store ip of user

--- urlModel.js
+++ urlModel.js
@@ -4,7 +4,20 @@ var Schema = mongoose.Schema;
 var urlModel = new Schema({
     url: String,
     segment: String,
-    created_at: Date
+    created_at: Date,
+    url_ip: String
+});
+
+// on every save, add the date
+urlModel.pre('save', function(next) {
+	// get the current date
+	var currentDate = new Date();
+
+  	// if created_at doesn't exist, add to that field
+  	if (!this.created_at)
+    	this.created_at = currentDate;
+
+  next();
 });
 
 var Url = mongoose.model("Url", urlModel);
